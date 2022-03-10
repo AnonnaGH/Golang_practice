5ab56e7faf206c5c1f769e4cd8df14b7407a037d
Anonna Ferdaus <44522472+AnonnaGH@users.noreply.github.com>
2022-03-10 15:50:36 +0600

    Create data_type.go

---
+++ data_type.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 12
+	var b bool = true
+	var c float32 = 3.14
+	var d string = "hi"
+	fmt.Println("int:", a)
+	fmt.Println("Boolian :", b)
+	fmt.Println("float :", c)
+	fmt.Println("string :", d)
+
+}
