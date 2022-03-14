8aff33b3618e300ab17efbc58d409f1bf3ffafff
Anonna Ferdaus <44522472+AnonnaGH@users.noreply.github.com>
2022-03-14 23:10:50 +0600

    Update array2.go

--- array2.go
+++ array2.go
@@ -4,7 +4,8 @@ import ("fmt")
 func main() {
   var arr1 = [...]int{1,2,3}
   arr2 := [...]int{4,5,6,7,8}
-
+  arr3 := [...]int{4,5,6,7,8,9}
   fmt.Println(arr1)
   fmt.Println(arr2)
+   fmt.Println(arr3)
 }
