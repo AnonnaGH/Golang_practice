a48c72d1435479f4d862619cdd9e122decf105fa
Anonna Ferdaus <44522472+AnonnaGH@users.noreply.github.com>
2022-03-23 20:37:32 +0600

    Create elseof3.go

---
+++ elseof3.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt")
+
+func main() {
+  x := 30
+  if x >= 10 {
+    fmt.Println("x is larger than or equal to 10.")
+  } else if x > 20
+    fmt.Println("x is larger than 20.")
+  } else {
+    fmt.Println("x is less than 10.")
+  }
+}
